711b576a4fe709f467490c0f7f6151164984c167
Alumno <Alumno@PC-08>
2022-10-06 12:16:03 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_NotePad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void colorDeTextoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
